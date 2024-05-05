1e9d426298466c29ec6eddb017c8f47c95b93d6f
Gurminder Boparai <gurminder_b100@hotmail.com>
2024-05-05 14:33:21 -0700

    add twitter handler to social media

--- src/social/socialHandler.ts
+++ src/social/socialHandler.ts
@@ -1,1 +1,15 @@
+import { sendTweet, uploadMedia } from "./twitter";
+import config from "../../config.json";
 
+/**
+ * Sends a tweet with optional media attachments.
+ * @param text - The text content of social media.
+ * @param media - An optional array of media file paths to attach to the tweet.
+ * @returns A promise that resolves when the tweet is sent.
+ */
+export async function send(text: string, media?: string[]): Promise<void> {
+    if (config.twitter.isActive) {
+        const mediaIds = media ? await Promise.all(media.map(uploadMedia)) : undefined;
+        await sendTweet(text, mediaIds);
+    }
+}
---
+++ src/social/twitter.ts
@@ -0,0 +1,43 @@
+
+import TwitterApi from "twitter-api-v2/dist/esm/client";
+import config from "../../config.json";
+import { TUploadableMedia } from "twitter-api-v2";
+const twitter = new TwitterApi({
+    appKey: config.twitter.appKey,
+    appSecret: config.twitter.appSecret,
+    accessToken: config.twitter.accessToken,
+    accessSecret: config.twitter.accessSecret,
+});
+
+/**
+ * Sends a tweet using the Twitter API.
+ * @param tweet - The content of the tweet.
+ * @param media - The media to be attached to the tweet. It can be a single media ID or an array of media IDs.
+ * @returns A Promise that resolves when the tweet is sent successfully, or rejects with an error if there's a problem.
+ */
+export async function sendTweet(tweet: string, media?: string | string[]): Promise<void> {
+    try {
+        if (media) {
+            await twitter.v1.tweet(tweet, { media_ids: media });
+        } else {
+            await twitter.v1.tweet(tweet);
+        }
+    } catch (error: any) {
+        console.error("Error sending tweet:", error.message as string);
+    }
+}
+
+/**
+ * Uploads media to Twitter.
+ * @param media - The media to upload.
+ * @returns A Promise that resolves to the media ID of the uploaded media, or an empty string if there's an error.
+ */
+export async function uploadMedia(media: TUploadableMedia): Promise<string> {
+    try {
+        const mediaData = await twitter.v1.uploadMedia(media, {}, true);
+        return mediaData.media_id_string;
+    } catch (error: any) {
+        console.error("Error uploading media:", error.message as string);
+        return "";
+    }
+}
